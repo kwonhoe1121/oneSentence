aa323e58be0f8e7ffc2432ed2a6775e2c962ac3f
kwonhoe1121 <kwonhoe1121@gmail.com>
2019-03-08 23:20:08 +0900

    delete onesentence dir

--- dahye/oneSentencePjt/src/main/java/com/one/sentence/search/controller/SearchController.java
+++ dahye/oneSentencePjt/src/main/java/com/one/sentence/search/controller/SearchController.java
@@ -14,42 +14,41 @@ import com.one.sentence.search.model.SearchModel;
 import com.one.sentence.search.service.SearchAladdinService;
 import com.one.sentence.search.service.SearchUserService;
 
-
 @Controller
 @RequestMapping("/search")
 public class SearchController {
 
 	@Autowired
 	private SearchAladdinService service;
-	
+
 	@Autowired
 	private SearchUserService serviceUser;
-	
-	@RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-	public String getQuery(HttpServletRequest request,Model model) throws Exception {
-		String query = (String)request.getParameter("search");
-		
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String getQuery(HttpServletRequest request, Model model) throws Exception {
+		String query = (String) request.getParameter("search");
+
 		Object search = service.getSearchModel(query);
-		
+
 		System.out.println("쿼리");
 		if (search != null) {
-		List<SearchModel> items = service.getSearchModel(query);
-		model.addAttribute("items", items);
-		System.out.println("검색완료");
-		return "search/searchresult";
+			List<SearchModel> items = service.getSearchModel(query);
+			model.addAttribute("items", items);
+			System.out.println("검색완료");
+			return "search/searchresult";
 		} else {
 			System.out.println("검색결과없음");
 			return "search/searchfail";
 		}
 	}
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public String getUserForm(HttpServletRequest request, Model model)  {
-		String userQuery = (String)request.getParameter("search");
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String getUserForm(HttpServletRequest request, Model model) {
+		String userQuery = (String) request.getParameter("search");
 		System.out.println("찍힘");
-	
+
 		Object search = serviceUser.selectUserList();
-		
+
 		if (search != null) {
 			List<String> selectUser = serviceUser.selectUserList();
 			System.out.println("검색완료");
@@ -59,5 +58,5 @@ public class SearchController {
 			return "search/searchfail";
 		}
 	}
-	
+
 }
