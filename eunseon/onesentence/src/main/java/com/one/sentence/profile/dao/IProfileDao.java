9670ac191f02f1d7c4478bdbb84f8aa361ebebac
1027 <1027@BIT05-07>
2019-03-08 10:30:36 +0900

    ..

--- eunseon/onesentence/src/main/java/com/one/sentence/profile/dao/IProfileDao.java
+++
@@ -1,8 +0,0 @@
-package com.one.sentence.profile.dao;
-
-import com.one.sentence.model.User_info;
-
-public interface IProfileDao {
-	public User_info selectUser_infoByuserIdx(int userIdx);
-	public int updateUser_info(int userIdx,String userPassword, String userName, String userIntroduction);
-}
