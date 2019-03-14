8492df8fda1e698bce6f998857ddec1966305a8b
cyes7126 <yoo712695@gmail.com>
2019-03-14 13:06:13 +0900

    second commit

---
+++ master/oneSentence_master/src/main/java/com/one/sentence/register/controller/RegisterController.java
@@ -0,0 +1,61 @@
+package com.one.sentence.register.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import com.one.sentence.common.vo.UserVo;
+import com.one.sentence.register.service.RegisterService;
+
+@Controller
+@SessionAttributes("UserVo")
+public class RegisterController {
+
+	@Inject
+	RegisterService service;
+
+	@RequestMapping(value = "/user/register", method = RequestMethod.POST)
+	public String requestRegistration(UserVo user, Model model, HttpSession session) {
+		System.out.println(user + "등록 전");
+//체크체
+		if (service.registerUser(user) != -1) {
+			service.registerUser(user);
+			session.setAttribute("User", user);
+
+		}
+		System.out.println(user + "return 전");
+		return "home";
+	}
+
+//	@RequestMapping(method = RequestMethod.GET)
+//	public String tempMethod() {
+//		return "/main";
+//	}
+
+	// 이메일 중복 체크 확인.
+	@RequestMapping("/emailCheck")
+	@ResponseBody
+	public Map<Object, Object> checkOverrapedEmail(@RequestBody String email) {
+
+		Map<Object, Object> map = new HashMap<>();
+		boolean chekUser = service.isUser(email);
+		map.put("chekUser", chekUser);
+
+		return map;
+	}
+
+	// 회원탈퇴
+//	@RequestMapping("/user")
+//	public String 
+
+}
---
+++ master/oneSentence_master/src/main/java/com/one/sentence/register/service/IRegisterService.java
@@ -0,0 +1,13 @@
+package com.one.sentence.register.service;
+
+import com.one.sentence.common.vo.UserVo;
+
+public interface IRegisterService {
+
+	public int registerUser(UserVo user);
+
+	public boolean isUser(String email);
+
+	public int withdrawUser(UserVo user);
+
+}
