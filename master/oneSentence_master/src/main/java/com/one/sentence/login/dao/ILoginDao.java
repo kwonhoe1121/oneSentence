4696632e78d0f0ca655b1d3222c51eebc23f235f
cyes7126 <yoo712695@gmail.com>
2019-03-14 13:10:25 +0900

    Revert "Merge branch 'eunsun1'"
    This reverts commit 7c627cafe5a3cfb06a162271df0b0db9e9f56fe3, reversing
    changes made to 10b6cf3f1d955e6d481a0c68c5eea1e656cb532d.

--- master/oneSentence_master/src/main/java/com/one/sentence/login/dao/ILoginDao.java
+++
@@ -1,9 +0,0 @@
-package com.one.sentence.login.dao;
-
-import com.one.sentence.common.vo.UserVo;
-
-public interface ILoginDao {
-
-	public UserVo selectUserByEmail(String email);
-
-}
--- master/oneSentence_master/src/main/java/com/one/sentence/login/service/ILoginService.java
+++
@@ -1,9 +0,0 @@
-package com.one.sentence.login.service;
-
-import com.one.sentence.common.vo.UserVo;
-
-public interface ILoginService {
-	public boolean isUser(String email);
-
-	public UserVo selectUser(String email);
-}
