1c1862f005d63bea6bef0a4ca6b2fe910a667054
kwonhoe1121 <kwonhoe1121@gmail.com>
2019-03-21 13:32:57 +0900

    resolve conflict of index.jsp

---
+++ master/oneSentence_master/src/main/java/com/one/sentence/login/interceptor/LoginIntercepter2.java
@@ -0,0 +1,28 @@
+package com.one.sentence.login.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class LoginIntercepter2 extends HandlerInterceptorAdapter {
+	// 로그인 상태에서 
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		HttpSession session = request.getSession(false);
+
+		if (session != null) {
+			Object obj = session.getAttribute("User");
+			if (obj != null) {
+				System.out.println("로그인 상태");
+				return true;
+			}
+		}
+
+		System.out.println("비로그인 상태");
+		response.sendRedirect(request.getContextPath() + "/login");
+		return false;
+	}
+}
