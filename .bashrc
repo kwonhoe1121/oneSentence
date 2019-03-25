a12a31ae15b4feddce1a9dcdb4263e577c41f889
kwonhoe1121 <kwonhoe1121@gmail.com>
2019-03-25 09:40:02 +0900

    added test Natural Language function

---
+++ .bashrc
@@ -0,0 +1,4 @@
+## Google Cloud
+export GOOGLE_CLOUD_SDK_PATH=/Users/kwonjaejin/dev/google-cloud-sdk
+export PATH=$PATH:$GOOGLE_CLOUD_SDK_PATH/bin
+
---
+++ master/oneSentence_master/src/main/java/com/one/sentence/naturallanguage/controller/NaturalLanguageController.java
@@ -0,0 +1,9 @@
+package com.one.sentence.naturallanguage.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class NaturalLanguageController {
+
+	
+}
