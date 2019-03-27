01087bf02f8b0cc9aa229f1bfd7c1fdfa9c6a8cd
1027 <1027@BIT05-07>
2019-03-27 17:33:33 +0900

    following 프로시저 구현

--- master/oneSentence_master/src/main/java/com/one/sentence/following/service/FollowingService.java
+++ master/oneSentence_master/src/main/java/com/one/sentence/following/service/FollowingService.java
@@ -1,10 +1,11 @@
 package com.one.sentence.following.service;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.one.sentence.following.dao.FollowingDao;
-import com.one.sentence.following.model.Following;
 
 @Service
 public class FollowingService implements IFollowingService {
@@ -12,19 +13,24 @@ public class FollowingService implements IFollowingService {
 	@Autowired
 	private FollowingDao dao;
 	
-	@Override
-	public Following checkFollowingState(Following following) {
-		return dao.selectFollower(following);
-	}
+//	@Override
+//	public Following checkFollowingState(Following following) {
+//		return dao.selectFollower(following);
+//	}
+//
+//	@Override
+//	public int startFollowing(Following following) {
+//		return dao.insertFollower(following);
+//	}
+//
+//	@Override
+//	public int unfollowing(Following following) {
+//		return dao.deleteFollower(following);
+//	}
 
 	@Override
-	public int startFollowing(Following following) {
-		return dao.insertFollower(following);
-	}
-
-	@Override
-	public int unfollowing(Following following) {
-		return dao.deleteFollower(following);
+	public int followingProc(Map<String, Object> followingMap) {
+		return dao.followingProc(followingMap);
 	}
 
 	
